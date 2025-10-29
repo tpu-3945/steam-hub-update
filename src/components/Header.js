fa4124fbb1268794c32da477ca99c931684713b1
Tueur_de_l€ak <vdsisaac410@gmail.com>
2025-10-29 12:12:46 +0100

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/Header.css';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="orbitron neon-title">STEAM HUB</h1>
+    <p className="header-subtitle">
+      Générateur de manifests et exploration d'AppID Steam, relax et immersif.
+    </p>
+  </header>
+);
+
+export default Header;
