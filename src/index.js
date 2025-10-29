f2379aadf8f03e29929977bff4d2b8ee3f440c53
Tueur_de_l€ak <vdsisaac410@gmail.com>
2025-10-29 12:33:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import './styles/animations.css';
+import './styles/animations.css'; // CHEMIN CORRIGÉ ICI
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
