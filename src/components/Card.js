d82b69156d2bd809723c41617b6005821d8efc25
Tueur_de_l€ak <vdsisaac410@gmail.com>
2025-10-29 12:42:00 +0100

    Delete src/components/Card.js

--- src/components/Card.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../assets/Card.css';
-
-const Card = ({ children, className = '' }) => {
-  return <div className={`card ${className}`}>{children}</div>;
-};
-
-export default Card;
